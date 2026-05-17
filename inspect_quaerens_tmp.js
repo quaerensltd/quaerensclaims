a6366f4ff8dcaf4a382caf6705297e889dc0d5e1
Jeroen Brussel <quaerensltd@consultant.com>
2026-05-17 15:44:34 +0200

    Mobile layout fix

---
+++ inspect_quaerens_tmp.js
@@ -0,0 +1,10 @@
+﻿const fs = require('fs');
+for (const f of ['public/index.html','public/category-property.html','public/category-travel.html']) {
+  const s = fs.readFileSync(f, 'utf8');
+  console.log('\n===== ' + f + ' =====');
+  for (const p of ['<nav','free-tools','Finance','card-grid','About Quaerens','footer']) {
+    const i = s.indexOf(p);
+    console.log('--- ' + p + ' ' + i + ' ---');
+    console.log(s.slice(Math.max(0, i - 350), i + 1200));
+  }
+}
